9f2781ac452cf667fd6ec567de37b7bc80f4c489
Keshav <keshavbiswa21@gmail.com>
2018-06-05 16:16:33 +0530

    Added devise authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include CurrentUserConcern
+  
+  def layout_by_resource
+    if devise_controller? && (resource_name == :user)  && action_name != 'edit' && action_name != 'update'
+      "devise"
+    else
+      "application"
+    end
+  end
+
 end
---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,13 @@
+module CurrentUserConcern 
+  extend ActiveSupport::Concern
+
+  def current_user
+    super || guest_user
+  end
+
+  def guest_user
+    guest = GuestUser.new
+    guest.email = "guest@example.com"
+    guest
+  end
+end
