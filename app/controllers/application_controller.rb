5ddb104c14c80955c4f095b8238307b2f6c3bdb1
Keshav <keshavbiswa21@gmail.com>
2018-06-05 16:53:54 +0530

    Fixed logout problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include CurrentUserConcern
-  
   def layout_by_resource
     if devise_controller? && (resource_name == :user)  && action_name != 'edit' && action_name != 'update'
       "devise"
