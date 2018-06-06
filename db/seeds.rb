7d92ece1f8daaa6223d982667e2ff17334577bc9
Keshav <keshavbiswa21@gmail.com>
2018-06-06 11:35:25 +0530

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: "admin@test.com",
+  password: "admin123",
+  password_confirmation: "admin123",
+  name: "Admin User",
+  roles: "site_admin"
+)
